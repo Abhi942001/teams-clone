f6e3f9c38b51e80adcc77e3031c54a8ae47ae9a1
abhi92001 <abhishek942001@gmail.com>
2021-07-13 20:12:43 +0530

    version 1 production ready

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,20 @@
+import dotenv from "dotenv"
+
 import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
+dotenv.config();
 
-const app= firebase.initializeApp({
-    apiKey: "AIzaSyAKXvBb5NSpfhQ9cSEBB5ioyHbLiWNUaQk",
-    authDomain: "teams-clone-6eba8.firebaseapp.com",
-    projectId: "teams-clone-6eba8",
-    storageBucket: "teams-clone-6eba8.appspot.com",
-    messagingSenderId: "640620991302",
-    appId: "1:640620991302:web:1509153b63108df6018b47"
-})
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID 
+  }
+const app = firebase.initializeApp(firebaseConfig);
 
 export const db = app.firestore();
 export const auth = app.auth();
